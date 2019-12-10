d45db53deebb1022fe20fe2dd2bfc68894974cbf
Liron Perel <liron.perel@sapiens.com>
2019-12-10 17:51:23 +0200

    Updates

---
+++ precache-manifest.8fad0be1ec88551667c1c2c3c9b5e8b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01823c31ca1eab668132e69411dbc590",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8d8c6f8a0bee148a098",
+    "url": "/static/css/main.0e65c5e7.chunk.css"
+  },
+  {
+    "revision": "f849dd31d6acdf2532f3",
+    "url": "/static/js/2.ab410fee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ab410fee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b8d8c6f8a0bee148a098",
+    "url": "/static/js/main.a1f3d095.chunk.js"
+  },
+  {
+    "revision": "950a31c340e15f144ec5",
+    "url": "/static/js/runtime-main.1a46b3d4.js"
+  }
+]);
