5a9daca67c373a2e10a0013de342e5e1fa215e85
Liron Perel <Lironp90@gmail.com>
2019-12-11 08:58:48 +0200

    Updates

---
+++ precache-manifest.74d621f79e973c2ec44c828fe178d3cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e8dc46239fbee78477fb1796c683cc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "41e8d8b9254713fa4116",
+    "url": "/portfolio/static/css/main.496d7671.chunk.css"
+  },
+  {
+    "revision": "f849dd31d6acdf2532f3",
+    "url": "/portfolio/static/js/2.ab410fee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.ab410fee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41e8d8b9254713fa4116",
+    "url": "/portfolio/static/js/main.e16c7b54.chunk.js"
+  },
+  {
+    "revision": "b0fe49fa4f46ef8ac931",
+    "url": "/portfolio/static/js/runtime-main.2c3c5d2a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.facc5014c91d3a93073f3e715a9223f7.js"
+  "/portfolio/precache-manifest.74d621f79e973c2ec44c828fe178d3cb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
