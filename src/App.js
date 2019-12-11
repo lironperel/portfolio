8211131c74a7d0a873c63f1f05ffe267c4f1586d
Liron Perel <Lironp90@gmail.com>
2019-12-11 15:35:49 +0200

    added skills

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <img id='my-photo' width='150px' src={require('./images/Me.jpg')} />
+      <div>
+        <img id='my-photo' width='150px' src={require('./images/Me.jpg')} />
+      </div>
+      <div id='my-title'>
+        Full-Stack Developer
+      </div>
+      <div id='pl-icons'>
+        <img className='anim-dur animated zoomInUp' width='150px' src={require('./images/HTMLCSSJS.png')} />
+        <div className='anim-dur animated zoomInUp delay-1s'>
+          <img width='100px' height='100px' src={require('./images/react.png')} />
+          <p style={{marginTop:'-15px'}}>ReactJS<br /> React-Native</p>
+        </div>
+        <img className='anim-dur animated zoomInUp delay-2s' width='100px' src={require('./images/nodejs.png')} />
+        <img className='anim-dur animated zoomInUp delay-3s' width='120px' src={require('./images/ExpressJS.png')} />
+      </div>
     </div>
   );
 }
